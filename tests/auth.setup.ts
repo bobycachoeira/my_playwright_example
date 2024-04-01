8896efd14a573652d91ccc8dd92d5b48fcf02e99
bobycachoeira <bobycachoeira@gmail.com>
2024-04-01 17:17:40 -0300

    Adicionado um teste e ajustado uma pasta de lugar

--- tests/auth.setup.ts
+++ tests/auth.setup.ts
@@ -1,6 +1,7 @@
 import { test as setup, expect } from '@playwright/test';
+import { log } from 'console';
 
-const authFile = 'playwright/.auth/user.json';
+const authFile = 'fixtures/.auth/user.json';
 
 setup('authenticate', async ({ page }) => {
   // Perform authentication steps. Replace these actions with your own.
@@ -8,15 +9,13 @@ setup('authenticate', async ({ page }) => {
   await page.locator('#email').fill('beltrano@qa.com.br');
   await page.locator('#password').fill('teste');
   await page.getByTestId('entrar').click();
-  // Wait until the page receives the cookies.
-  //
-  // Sometimes login flow sets cookies in the process of several redirects.
-  // Wait for the final URL to ensure that the cookies are actually set.
+  
   await page.waitForURL('https://front.serverest.dev/admin/home');
-  // Alternatively, you can wait until the page reaches a state where all cookies are set.
-  await expect(page.getByText('Bem Vindo Rogelio.Ward3', { exact: true })).toBeVisible();
+  await expect(page.getByText('Bem Vindo Fulano da Silva', { exact: true })).toBeVisible();
 
-  // End of authentication steps.
-
+ 
+//Guardar o estado de autenticação no arquivo .auth/user.json
   await page.context().storageState({ path: authFile });
+  console.log('authFile:', authFile);
+  console.log('Context storageState:', await page.context().storageState());
 });
--- tests/example.spec.ts
+++ tests/example.spec.ts
@@ -1,25 +1,15 @@
 import { test, expect } from '@playwright/test';
 
-// test('is login page', async ({ page }) => {
-//   await page.goto('/');
-
-//   // Expect a title "to contain" a substring.
-//   await expect(page).toHaveTitle('Front - ServeRest');
-//   await expect(page).toHaveURL('/login')
-// });
-
-// test('do login', async ({ page }) => {
-//   await page.goto('/login');
-//   await page.locator('#email').fill('Damion99@yahoo.com');
-//   await page.locator('#password').fill('oP1QtzShL5IJp__');
-//   await page.getByTestId('entrar').click();
-
-//   await expect(page.getByText('Bem Vindo Rogelio.Ward3', { exact: true })).toBeVisible();
-
-// });
-
 test('do something', async ({ page }) => {
   await page.goto('/admin/home');
   await expect(page.getByText('Bem Vindo Fulano da Silva', { exact: true })).toBeVisible();
 
-});
+}
+);
+
+test('do something two', async ({ page }) => {
+  await page.goto('/admin/home');
+  await page.getByTestId('cadastrarUsuarios').click();
+  await expect(page.getByText('Cadastro de usuários', { exact: true })).toBeVisible();
+}
+);
